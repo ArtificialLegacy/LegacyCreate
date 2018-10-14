f3918512f14b89fabc82344b0f55092df1e8d891
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-14 15:30:51 -0400

    Finished constructor and check method

--- base/buttons.js
+++ base/buttons.js
@@ -1,15 +1,40 @@
 class Buttton {
-  constructor(tempX, tempY, tempWidth, tempHeight){
+  constructor(tempX, tempY, tempWidth, tempHeight, tempFunction){
+    this.function = tempFunction;
     this.positions = {
       "x": tempX,
       "y": tempY,
-    }
+    };
     this.shape = {
       "width": tempWidth,
       "height": tempHeight,
       "vertexes": {
-        
+        "top-left": {
+          "x": tempX,
+          "y": tempY,
+        },
+        "top-right": {
+          "x": tempX+tempWidth,
+          "y": tempY,
+        },
+        "bottom-left": {
+          "x": tempX,
+          "y": tempY+tempHeight,
+        },
+        "bottom-right": {
+          "x": tempX+tempWidth,
+          "y": tempY+tempHeight,
+        }
       },
+    };
+  }
+  check(tempX, tempY){
+    if(tempX >= this.shape.vertexes.top-left.x && tempX <= this.shape.vertexes.top-right.x){
+      if(tempY >= this.shape.vertexes.top-left.y && tempY <= this.shape.vertexes.bottom-left.y){
+        this.function();
+      }
     }
   }
 }
+
+export default Button;
