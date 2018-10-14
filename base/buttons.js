bb03e7057b6cb77c89aef73522cdf68639011513
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-14 14:40:46 -0400

    started adding class constructor

--- base/buttons.js
+++ base/buttons.js
@@ -1,1 +1,15 @@
-
+class Buttton {
+  constructor(tempX, tempY, tempWidth, tempHeight){
+    this.positions = {
+      "x": tempX,
+      "y": tempY,
+    }
+    this.shape = {
+      "width": tempWidth,
+      "height": tempHeight,
+      "vertexes": {
+        
+      },
+    }
+  }
+}
