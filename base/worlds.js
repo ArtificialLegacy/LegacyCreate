24d213aa205a00ae372c00f330f0dc3670f8783e
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-15 18:03:47 -0400

    added constructor and method

--- base/worlds.js
+++ base/worlds.js
@@ -1,1 +1,20 @@
+class World {
+  constructor(tempName){
+    this.name = tempName;
+    this.layers = [];
+  }
+  add(tempLayer, tempDebug = false){
+    this.instances.push(tempLayer);
+    if(tempDebug == false) console.log(`Added layer${tempLayer.name} to layer position ${this.layers.length-1}.`);
+  }
+  remove(tempID){
+    delete this.layers[tempID];
+  }
+  pop(tempID){
+    object = this.layers[tempID];
+    delete this.layers[tempID];
+    this.layers.unshift(object);
+  }
+}
 
+export default World;
