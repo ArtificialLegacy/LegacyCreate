dcdf2348620dbc243b1463d617e4f736cce9debd
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-15 18:18:26 -0400

    started to add methods

--- base/engine.js
+++ base/engine.js
@@ -1,7 +1,18 @@
-import Instance from '.instances.js';
+import Instance from './instances.js';
+import Layer from './layers.js';
+import World from './worlds.js';
 
 class Engine {
   constructor(){
+    
+  }
+  createInstant(){
+    
+  }
+  createLayer(){
+    
+  }
+  createWorld(){
     
   }
 }
