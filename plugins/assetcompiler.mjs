1b96e9a7339f0a45280cf70a2ce1e5b5df478234
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-15 12:52:11 -0400

    started adding compiler

--- plugins/assetcompiler.mjs
+++ plugins/assetcompiler.mjs
@@ -1,1 +1,7 @@
-
+// Requires p5.js
+var assets = [];
+async function compileAssets(tempAssets=[], tempCallback=()=>{}){
+  for(i=0; i<tempAssets.length; i++){
+    assets.push(loadImage(tempAssets[i]));
+  }
+}
