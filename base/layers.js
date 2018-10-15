bab7b6db39dc57c86f487eed8688149127d5086f
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-15 17:51:51 -0400

    started adding methods

--- base/layers.js
+++ base/layers.js
@@ -1,1 +1,20 @@
+class Layer {
+  constructor(tempName){
+    this.name = tempName;
+    this.instances = [];
+  }
+  add(tempObject, tempDebug = false){
+    this.instances.push(tempObject);
+    if(tempDebug == false) console.log(`Added object ${tempObject.name} to layer position ${this.instances.length-1}.`);
+  }
+  remove(tempID){
+    delete this.instances[tempID];
+  }
+  pop(tempID){
+    object = this.instances[tempID];
+    delete this.instances[tempID];
+    this.instances.unshift(object);
+  }
+}
 
+export default Layer;
