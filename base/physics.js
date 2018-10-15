d11688c119428269462b19b18c3351b2fa290da6
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-15 18:51:30 -0400

    started to add physics

--- base/physics.js
+++ base/physics.js
@@ -1,1 +1,10 @@
-
+async function check(tempObject1, tempObject2){
+  let x1 = tempObject2.position.x;
+  let y1 = tempObject2.position.y;
+  let w1 = tempObject2.shape.width;
+  let h1 = tempObject2.shape.height;
+  let x2 = tempObject2.position.x;
+  let y2 = tempObject2.position.y;
+  let w2 = tempObject2.shape.width;
+  let h2 = tempObject2.shape.height;
+}
