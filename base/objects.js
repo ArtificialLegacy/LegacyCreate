746f5c20a346cfb320de3ac0ca30d417ad6b43aa
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-15 16:03:22 -0400

    Started adding the constructor

--- base/objects.js
+++ base/objects.js
@@ -1,1 +1,31 @@
-
+class Instance {
+  constructor(tempX, tempY, tempWidth, tempHeight, tempName){
+    this.name = tempName;
+    this.position = {
+      "x": tempX,
+      "y": tempY,
+    };
+    this.shape = {
+      "width": tempWidth,
+      "height": tempHeight,
+      "vertexes": {
+        "top-left": {
+          "x": tempX,
+          "y": tempY,
+        },
+        "top-right": {
+          "x": tempX+tempWidth,
+          "y": tempY,
+        },
+        "bottom-left": {
+          "x": tempX,
+          "y": tempY+tempHeight,
+        },
+        "bottom-right": {
+          "x": tempX+tempWidth,
+          "y": tempY+tempHeight,
+        },
+      },
+    };
+  }
+}
