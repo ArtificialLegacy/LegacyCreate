c2be71b54122d0d1207e516cefdde306abfd52a5
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-16 12:50:15 -0400

    added box collisions

--- base/physics.js
+++ base/physics.js
@@ -1,4 +1,4 @@
-async function check(tempObject1, tempObject2){
+async function checkColl(tempObject1, tempObject2){
   let x1 = tempObject2.position.x;
   let y1 = tempObject2.position.y;
   let w1 = tempObject2.shape.width;
@@ -7,4 +7,25 @@ async function check(tempObject1, tempObject2){
   let y2 = tempObject2.position.y;
   let w2 = tempObject2.shape.width;
   let h2 = tempObject2.shape.height;
+  
+  if(x1 >= x2 && x1 <= x2+w2){
+    if(y1 >= y2 && y1 <= y2+h2){
+      
+    }
+  } // Top left
+  if(x1+w1 >= x2 && x1+w1 <= x2+w2){
+   if(y1 >= y2 && y1 <= y2+h2){
+     
+   }
+  } // Top right
+  if(x1 >= x2 && x1 <= x2+w2){
+   if(y1+h1 >= y2 && y1+h1 >= y2+h2){
+     
+   }
+  } // Bottom left
+  if(x1+w1 >= x2 && x1+w1 <= x2+w2){
+    if(y1+h1 >= y2 && y1+h1 <= y2+h2){
+      
+    }
+  } // Bottom right
 }
