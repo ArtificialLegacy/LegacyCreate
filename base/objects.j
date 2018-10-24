5c8ca9d91e0c9d27932b73d4bcc7b64cc8e9c38b
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-24 13:35:52 -0400

    Added parent

--- base/objects.j
+++
@@ -1,7 +0,0 @@
-class Object {
-  constructor(tempName, tempID, tempHasPhysics){
-    this.name = tempName;
-    this.id = tempID;
-    this.hasPhysics = tempHasPhysics;
-  }
-}
---
+++ base/objects.js
@@ -0,0 +1,9 @@
+class Object {
+  constructor(tempName, tempParent=false, tempHasPhysics, tempIsVisable, tempSprite=null){
+    this.name = tempName;
+    this.parent = tempParent;
+    this.hasPhysics = tempHasPhysics;
+    this.isVisable = tempIsVisable;
+    this.sprite = tempSprite;
+  }
+}
