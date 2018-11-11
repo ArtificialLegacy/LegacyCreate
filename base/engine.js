6dd0d0f3ad2511a5d000a8d77f65ac5e0216b248
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-11-10 19:02:08 -0500

    Update engine.js

--- base/engine.js
+++ base/engine.js
@@ -4,19 +4,24 @@ import Physics from './physics.js';
 import Instance from './instances.js';
 import Layer from './layers.js';
 import World from './worlds.js';
+import Objects from './objects.js';
 
 class Engine {
   constructor(){
-    
+    this.objects = {};
   }
-  createInstant(tempX, tempY, tempWidth, tempHeight, tempName){
-    return new Instance(tempX, tempY, tempWidth, tempHeight, tempName);
+  createInstance(tempID, tempTemplate, tempX, tempY, tempWidth, tempHeight){
+    return new Instance(tempID, tempTemplate, tempX, tempY, tempWidth, tempHeight);
   }
   createLayer(tempName){
     return new Layer(tempName);
   }
   createWorld(tempName){
     return new World(tempName);
+  }
+  createObject(tempName, tempParent=undefined, tempHasPhysics, tempIsVisable, tempSprite=undefined){
+    this.objects[tempName] = new Objects(tempName, tempParent, tempHasPhysics, tempIsVisable, tempSprite);
+    return new Objects(tempName, tempParent, tempHasPhysics, tempIsVisable, tempSprite);
   }
 }
 
