f03fc5b92e26e64c5a9fec12b761c573f0dc1173
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2019-01-10 18:30:49 -0500

    renamed methods

--- base/layers.js
+++ base/layers.js
@@ -3,14 +3,14 @@ class Layer {
     this.name = tempName;
     this.instances = [];
   }
-  add(tempObject, tempDebug = false){
+  addInstance(tempObject, tempDebug = false){
     this.instances.push(tempObject);
-    if(tempDebug == false) console.log(`Added object ${tempObject.name} to layer position ${this.instances.length-1}.`);
+    if(tempDebug) console.log(`Added object ${tempObject.name} to layer position ${this.instances.length-1}.`);
   }
-  remove(tempID){
+  removeInstance(tempID){
     delete this.instances[tempID];
   }
-  pop(tempID){
+  popInstance(tempID){
     object = this.instances[tempID];
     delete this.instances[tempID];
     this.instances.unshift(object);
