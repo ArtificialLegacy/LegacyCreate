6502979d6781af37a980badff38b4bfcd6d75942
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2019-01-11 18:05:47 -0500

    updated some thingys

--- base/layers.js
+++ base/layers.js
@@ -6,14 +6,16 @@ class Layer {
   addInstance(tempObject, tempDebug = false){
     this.instances.push(tempObject);
     if(tempDebug) console.log(`Added object ${tempObject.name} to layer position ${this.instances.length-1}.`);
+    return this.instances.length-1;
   }
   removeInstance(tempID){
     delete this.instances[tempID];
   }
   popInstance(tempID){
     object = this.instances[tempID];
-    this.instances.splice(tempID);
+    this.instances.splice(tempID, 1);
     this.instances.unshift(object);
+    return 0;
   }
 }
 
