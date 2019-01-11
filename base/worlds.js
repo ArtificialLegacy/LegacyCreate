dc2455cea9978c5b8c9f022fd0edcd42343032d9
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2019-01-11 12:50:11 -0500

    fixed popLayer()

--- base/worlds.js
+++ base/worlds.js
@@ -5,14 +5,14 @@ class World {
   }
   addLayer(tempLayer, tempDebug = false){
     this.instances.push(tempLayer);
-    if(tempDebug) console.log(`Added layer${tempLayer.name} to layer position ${this.layers.length-1}.`);
+    if(tempDebug) console.log(`Added layer ${tempLayer.name} to layer position ${this.layers.length-1}.`);
   }
   removeLayer(tempID){
     delete this.layers[tempID];
   }
   popLayer(tempID){
     object = this.layers[tempID];
-    delete this.layers[tempID];
+    this.layers.splice(tempID);
     this.layers.unshift(object);
   }
 }
