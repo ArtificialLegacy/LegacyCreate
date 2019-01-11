44897658b934d29e6b636baf7ef7e44c382e19a3
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2019-01-11 18:04:41 -0500

    added updateLayer()

--- base/worlds.js
+++ base/worlds.js
@@ -4,16 +4,20 @@ class World {
     this.layers = [];
   }
   addLayer(tempLayer, tempDebug = false){
-    this.instances.push(tempLayer);
+    this.layers.push(tempLayer);
     if(tempDebug) console.log(`Added layer ${tempLayer.name} to layer position ${this.layers.length-1}.`);
+    return this.layers.length-1;
   }
   removeLayer(tempID){
     delete this.layers[tempID];
   }
   popLayer(tempID){
     object = this.layers[tempID];
-    this.layers.splice(tempID);
+    this.layers.splice(tempID, 1);
     this.layers.unshift(object);
+  }
+  updateLayer(tempID, tempLayer){
+    this.layers[tempID] = tempLayer;
   }
 }
 
